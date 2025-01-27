34e06ff1efdd2b521887f1eb675df4f7f413772c
fmotzet <felix.motzet@gmail.com>
2025-01-27 17:12:09 +0100

    updated to version 3.20

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite-control-3.17.0.exe'
+$url        = 'https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_3.20.0.exe'
+
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +12,7 @@ $packageArgs = @{
 
   softwareName  = 'Focusrite-Control*' 
 
-  checksum      = '3699e24a484b8995ddd2313e4d67fe6c346a76604d7bba316f7a543b0afed9e8'
+  checksum      = '3d4e9aee3243293656ced425ab34d682bb6d754f5105b56e9a3d2f996d75b6c0'
   checksumType  = 'sha256'
 
   silentArgs   = '/SP- /VERYSILENT /NORESTART /SUPRESSMSGBOXES'
